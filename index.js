b3a14c742cdaa9893a0ff066b9d849f2d3f7e808
Mohamed <opvxgame@gmail.com>
2023-10-23 03:04:51 +0200

    a

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require("express")();
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
 });
+
+module.exports = app;
